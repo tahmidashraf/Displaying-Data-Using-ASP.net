6531582e25c43a7a65bcff463556cdb07b2fefa1
Md. Tahmid Ashraf Chowdhury <62513943+tahmidashraf@users.noreply.github.com>
2023-09-18 11:10:17 +0600

    commit

---
+++ DisplayData/DAL/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DisplayData.Models.DBEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DisplayData.DAL
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
